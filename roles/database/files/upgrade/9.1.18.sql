60b924fcfece1b8fd10515d49550cc5f54d37d32
Yannik Hayn <yannik@hayn.de>
2026-07-09 23:23:59 +0200

    fix(db): wrong ids in upgrade script

--- roles/database/files/upgrade/9.1.18.sql
+++ roles/database/files/upgrade/9.1.18.sql
@@ -1,5 +1,5 @@
 INSERT INTO stm_dev_typ (dev_typ_id,dev_typ_name,dev_typ_version,dev_typ_manufacturer,dev_typ_predef_svc,dev_typ_is_multi_mgmt,dev_typ_is_mgmt,is_pure_routing_device)
 VALUES
-    (32, 'Generic Firewall Management', '', null, '', false, true, false),
-    (33, 'Generic Firewall Gateway', '', null, '', false, false, false)
+    (30, 'Generic Firewall Management', '', null, '', false, true, false),
+    (31, 'Generic Firewall Gateway', '', null, '', false, false, false)
 ON CONFLICT (dev_typ_id) DO NOTHING;
