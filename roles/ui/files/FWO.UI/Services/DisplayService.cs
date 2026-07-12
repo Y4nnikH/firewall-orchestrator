4989b61e2be5fadbdbfda582e83459a7acce0e21
abarz722 <achim.barz@gmx.de>
2026-07-12 13:27:41 +0200

    split + enhance Wf Action Settings

--- roles/ui/files/FWO.UI/Services/DisplayService.cs
+++ roles/ui/files/FWO.UI/Services/DisplayService.cs
@@ -1,4 +1,5 @@
 using FWO.Config.Api;
+using FWO.Data.Workflow;
 using Microsoft.AspNetCore.Components;
 
 
@@ -23,6 +24,28 @@ namespace FWO.Ui.Services
             string objIconToDisplay = objIcon != "" ? $" <span class=\"{objIcon}\"/>" : "";
             string uniconified = $"<span {@tooltip}/>{@userConfig.GetText(text)}</span>";
             return (MarkupString)(userConfig.ModIconify ? iconToDisplay + iconTextPart + objIconToDisplay : uniconified);
+        }
+
+        public static string DisplayState(UserConfig userConfig, WfState state)
+        {
+            if (state.Id == -1)
+            {
+                return userConfig.GetText("automatic");
+            }
+
+            if (state.Id == -2)
+            {
+                return userConfig.GetText("Conditional");
+            }
+
+            return state.Name;
+        }
+
+        public static string DisplayStateWithId(UserConfig userConfig, WfState state)
+        {
+            return state.Id < 0
+                ? DisplayState(userConfig, state)
+                : $"{state.Name} ({state.Id})";
         }
     }
 }
